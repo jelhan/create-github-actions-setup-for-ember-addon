87fecd20d2d746b4efaa0690beca5db6a3773ff8
Jeldrik Hanschke <admin@jhanschke.de>
2020-11-22 15:54:46 +0100

    ember try scenarios should be dynamic

--- src/index.ts
+++ src/index.ts
@@ -18,8 +18,22 @@ const templateFile = path.join(
   'workflows',
   'ci.yml'
 );
-const data = {};
-const options = {};
+const data = {
+  emberTryScenarios: {
+    required: [
+      'ember-lts-3.16',
+      'ember-lts-3.20',
+      'ember-release',
+      'ember-beta',
+      'ember-default-with-jquery',
+      'ember-classic',
+    ],
+    allowedToFail: ['ember-canary', 'embroider-tests'],
+  },
+};
+const options = {
+  // debug: true,
+};
 
 ejs.renderFile(templateFile, data, options, function (err, str) {
   if (err) {
